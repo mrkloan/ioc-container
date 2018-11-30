ddebcacca25bbf2adec5d2a53ababc8c75096ed5
Valentin Fries <contact@fries.io>
2018-11-30 15:33:55 +0100

    Removed unused register factory methods

--- src/main/java/io/fries/ioc/RegistrationContainer.java
+++ src/main/java/io/fries/ioc/RegistrationContainer.java
@@ -6,14 +6,10 @@ import io.fries.ioc.instantiator.Instantiator;
 import io.fries.ioc.registry.Registrable;
 import io.fries.ioc.registry.RegistrableBuilder;
 import io.fries.ioc.registry.Registry;
-import io.fries.ioc.registry.managed.ManagedRegistrable;
-import io.fries.ioc.registry.proxy.ProxyRegistrable;
-import io.fries.ioc.registry.supplied.SuppliedRegistrable;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Parameter;
 import java.util.List;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import static java.util.Arrays.stream;
@@ -30,41 +26,6 @@ public class RegistrationContainer {
 
     static RegistrationContainer of(final Instantiator instantiator, final Registry registry) {
         return new RegistrationContainer(instantiator, registry);
-    }
-
-    @SuppressWarnings("WeakerAccess")
-    public RegistrationContainer register(final Id id, final Supplier<?> instanceSupplier) {
-        final SuppliedRegistrable supplier = SuppliedRegistrable.of(id, instanceSupplier);
-        return register(supplier);
-    }
-
-    @SuppressWarnings("WeakerAccess")
-    public RegistrationContainer register(final Class<?> type) {
-        final Id id = Id.of(type);
-        return register(id, type);
-    }
-
-    @SuppressWarnings("WeakerAccess")
-    public RegistrationContainer register(final Id id, final Class<?> type) {
-        final List<Id> dependencies = inferDependenciesFrom(type);
-        return register(id, type, dependencies);
-    }
-
-    @SuppressWarnings("WeakerAccess")
-    public RegistrationContainer register(final Id id, final Class<?> type, final List<Id> dependencies) {
-        final ManagedRegistrable registrable = ManagedRegistrable.of(id, type, dependencies);
-        return register(registrable);
-    }
-
-    @SuppressWarnings("WeakerAccess")
-    public RegistrationContainer register(final Id id, final Class<?> interfaceType, final Class<?> type, final List<Id> dependencies) {
-        final ProxyRegistrable proxy = ProxyRegistrable.of(id, interfaceType, type, dependencies);
-        return register(proxy);
-    }
-
-    private RegistrationContainer register(final Registrable registrable) {
-        registry = registry.add(registrable);
-        return this;
     }
 
     @SuppressWarnings("WeakerAccess")
--- src/test/java/io/fries/ioc/RegistrationContainerTest.java
+++ src/test/java/io/fries/ioc/RegistrationContainerTest.java
@@ -6,9 +6,6 @@ import io.fries.ioc.instantiator.Instantiator;
 import io.fries.ioc.registry.Registrable;
 import io.fries.ioc.registry.RegistrableBuilder;
 import io.fries.ioc.registry.Registry;
-import io.fries.ioc.registry.managed.ManagedRegistrable;
-import io.fries.ioc.registry.proxy.ProxyRegistrable;
-import io.fries.ioc.registry.supplied.SuppliedRegistrable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,10 +14,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
-import java.util.function.Supplier;
 
 import static java.util.Arrays.asList;
-import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
@@ -39,45 +34,6 @@ class RegistrationContainerTest {
     @BeforeEach
     void setUp() {
         this.registrationContainer = RegistrationContainer.of(instantiator, registry);
-    }
-
-    @Test
-    @DisplayName("register a component supplier")
-    void should_register_a_component_supplier() {
-        final Id id = mock(Id.class);
-        final Supplier<Object> instanceSupplier = () -> mock(Object.class);
-        final SuppliedRegistrable supplier = SuppliedRegistrable.of(id, instanceSupplier);
-
-        registrationContainer.register(id, instanceSupplier);
-
-        verify(registry).add(supplier);
-    }
-
-    @Test
-    @DisplayName("register a managed registrable")
-    void should_register_a_managed_registrable() {
-        final Id id = mock(Id.class);
-        final Class<?> type = Object.class;
-        final List<Id> dependencies = emptyList();
-        final ManagedRegistrable registrable = ManagedRegistrable.of(id, type, dependencies);
-
-        registrationContainer.register(id, type, dependencies);
-
-        verify(registry).add(registrable);
-    }
-
-    @Test
-    @DisplayName("register a component proxy")
-    void should_register_a_component_proxy() {
-        final Id id = mock(Id.class);
-        final Class<?> interfaceType = Supplier.class;
-        final Class<?> type = Object.class;
-        final List<Id> dependencies = emptyList();
-        final ProxyRegistrable proxy = ProxyRegistrable.of(id, interfaceType, type, dependencies);
-
-        registrationContainer.register(id, interfaceType, type, dependencies);
-
-        verify(registry).add(proxy);
     }
 
     @Test
