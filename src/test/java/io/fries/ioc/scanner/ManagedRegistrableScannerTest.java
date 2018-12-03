101421108209ff1af1cb1b37f0ab1288701eb5fb
Valentin Fries <contact@fries.io>
2018-12-03 11:59:42 +0100

    ManagedRegistrableScanner should create a managed registrable with a non identified dependency

--- src/test/java/io/fries/ioc/scanner/ManagedRegistrableScannerTest.java
+++ src/test/java/io/fries/ioc/scanner/ManagedRegistrableScannerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import testable.NovelBook;
+import testable.stories.ScienceFictionStory;
 
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,6 +30,17 @@ class ManagedRegistrableScannerTest {
         final Registrable registrable = managedRegistrableScanner.createRegistrable(type);
 
         final ManagedRegistrable expected = ManagedRegistrable.of(Id.of("NovelBook"), NovelBook.class, singletonList(Id.of("FantasyStory")));
+        assertThat(registrable).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("create a ManagedRegistrable with an identified dependency")
+    void should_create_a_managed_registrable_with_a_non_identified_dependency() {
+        final Class<?> type = ScienceFictionStory.class;
+
+        final Registrable registrable = managedRegistrableScanner.createRegistrable(type);
+
+        final ManagedRegistrable expected = ManagedRegistrable.of(Id.of("ScienceFictionStory"), ScienceFictionStory.class, singletonList(Id.of("Plot")));
         assertThat(registrable).isEqualTo(expected);
     }
 }
---
+++ src/test/java/testable/stories/ScienceFictionStory.java
@@ -0,0 +1,12 @@
+package testable.stories;
+
+import testable.stories.plots.Plot;
+
+public class ScienceFictionStory implements Story {
+
+    private final Plot plot;
+
+    public ScienceFictionStory(final Plot plot) {
+        this.plot = plot;
+    }
+}
